75504d60f7150290fabe5685ec8166131da16e2d
Mateo Marchisone <mateo.marchisone@gmail.com>
2024-10-11 18:39:34 -0300

    Lab 3 completo

--- Algoritmos_y_Estructura_de_Datos_I/Laboratorio/Practico3/ejercicio1.c
+++ Algoritmos_y_Estructura_de_Datos_I/Laboratorio/Practico3/ejercicio1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void ejercicio1() {
     //  Declaración de variables
@@ -45,6 +46,177 @@ void ejercicio3() {
     printf("Resultados: c = %d, d = %d\n", c, d);
 }
 
+void ejercicio5() {
+    int x, y;
+
+    printf("Ingrese un valor x\n");
+    scanf("%d", &x);
+
+    printf("Ingrese un valor y\n");
+    scanf("%d", &y);
+
+    if (x >= y) {
+        x = 0;
+    } else {
+        x = 2;
+    }
+
+    printf("El resultado de x es: %d\n", x);
+}
+
+void ejercicio6() {
+    int x, y, z, m;
+
+    printf("Ingrese un valor x\n");
+    scanf("%d", &x);
+
+    printf("Ingrese un valor y\n");
+    scanf("%d", &y);
+
+    printf("Ingrese un valor z\n");
+    scanf("%d", &z);
+
+    printf("Ingrese un valor m\n");
+    scanf("%d", &m);
+
+    if (x < y) {
+        m = x;
+    } else if (x >= y) {
+        m = y;
+    }
+
+    if (m < z) {
+        ;
+    } else if (m >= z) {
+        m = z;
+    }
+
+    printf("El resultado de m es: %d\n", m);
+}
+
+void ejercicio7a() {
+    int i;
+
+    printf("Ingrese un valor i\n");
+    scanf("%d", &i);
+
+    while (i != 0) {
+        i--;
+        printf("El valor de i es: %d\n", i);
+    }
+}
+
+void ejercicio7b() {
+    int i;
+
+    printf("Ingrese un valor i\n");
+    scanf("%d", &i);
+
+    while (i != 0) {
+        i = 0;
+        printf("El valor de i es: %d\n", i);
+    }
+}
+
+void ejercicio7c() {
+    int i;
+
+    printf("Ingrese un valor i\n");
+    scanf("%d", &i);
+
+    while (i < 0) {
+        i--;
+        printf("El valor de i es: %d\n", i);
+    }
+}
+
+void ejercicio8a() {
+    int x, y;
+    int i = 0;
+
+    printf("Ingrese un valor x\n");
+    scanf("%d", &x);
+
+    printf("Ingrese un valor y\n");
+    scanf("%d", &y);
+
+    while (x >= y) {
+        x = x - y;
+        i++;
+        printf("El valor de x es: %d\n", x);
+        printf("El valor de i es: %d\n", i);
+    }
+}
+
+void ejercicio8b() {
+    int x;
+    int i = 2;
+    int res = true;
+
+
+    printf("Ingrese un valor x\n");
+    scanf("%d", &x);
+
+    while (i < x && res) {
+        res = res && (x % i != 0);
+        i++;
+        printf("Res: %d\n", res);
+        printf("i: %d\n", i);
+    }
+}
+
+void ejercicio9a() {
+    int i = 0;
+    int s = 0;
+
+    const int array[4];
+
+    printf("Ingrese el primer valor de la lista\n");
+    scanf("%d", &array[0]);
+
+    printf("Ingrese el segundo valor de la lista\n");
+    scanf("%d", &array[1]);
+
+    printf("Ingrese el tercer valor de la lista\n");
+    scanf("%d", &array[2]);
+
+    printf("Ingrese el cuarto valor de la lista\n");
+    scanf("%d", &array[3]);
+
+    while (i < 4) {
+        s = s + array[i];
+        i++;
+    }
+    printf("La suma de los elementos de la lista es %d \n", s);
+}
+
+void ejercicio9b() {
+    const int array[4];
+    int i = 0;
+    int c = 0;
+
+    printf("Ingrese el primer valor de la lista\n");
+    scanf("%d", &array[0]);
+
+    printf("Ingrese el segundo valor de la lista\n");
+    scanf("%d", &array[1]);
+
+    printf("Ingrese el tercer valor de la lista\n");
+    scanf("%d", &array[2]);
+
+    printf("Ingrese el cuarto valor de la lista\n");
+    scanf("%d", &array[3]);
+
+    while (i < 4) {
+        if (array[i] > 0) {
+            c++;
+        } else if (array[i] <= 0) {
+        }
+        i++;
+    }
+    printf("Hay %d numeros positivos\n", c);
+}
+
 int main() {
-    ejercicio3();
+    ejercicio9a();
 }

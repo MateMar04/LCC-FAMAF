45d40a3af129c01fc75d96db05a5fa2f762e46eb
Mateo Marchisone <mateo.marchisone@gmail.com>
2025-03-22 10:14:34 -0300

    ej1 lab 1

---
+++ Algoritmos_y_Estructuras_de_Datos_II/lab1/ej1/sort.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "array_helpers.h"
+#include "sort_helpers.h"
+#include "sort.h"
+
+
+static void insert(int a[], unsigned int i, unsigned int length) {
+    int key = a[i];
+    int j = i - 1;
+
+    while (j >= 0 && a[j] > key) {
+        swap(a, j, j + 1);
+        j--;
+    }
+    a[j + 1] = key;
+    array_dump(a, length);
+}
+
+void insertion_sort(int a[], unsigned int length) {
+    for (unsigned int i = 1; i < length; ++i) {
+        insert(a, i, length);
+    }
+}

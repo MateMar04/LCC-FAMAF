11da32cf83ede1e8f37da319a43cd756234268bc
Mateo Marchisone <mateo.marchisone@gmail.com>
2025-04-03 17:54:22 -0300

    Refactor quicksort implementation to use fixstring type for improved string handling

--- Algoritmos_y_Estructuras_de_Datos_II/lab1/ej5/sort.c
+++ Algoritmos_y_Estructuras_de_Datos_II/lab1/ej5/sort.c
@@ -8,19 +8,22 @@
 #include "sort.h"
 #include "fixstring.h"
 
-
-static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
+static unsigned int partition(fixstring a[], unsigned int izq, unsigned int der) {
     unsigned int ppiv = izq;
     unsigned int i = izq + 1;
     unsigned int j = der;
 
     while (i <= j) {
-        if (a[i] <= a[ppiv]) {
+        if (fstring_less_eq(a[i], a[ppiv])) {
             i++;
-        } else if (a[j] > a[ppiv]) {
+        }
+        else if (fstring_less_eq(a[ppiv], a[j])) {
             j--;
-        } else if (a[i] > a[ppiv] && a[j] < a[ppiv]) {
-            swap(a, i,j);
+        }
+        else {
+            swap(a, i, j);
+            i++;
+            j--;
         }
     }
     swap(a, ppiv, j);
@@ -28,19 +31,13 @@ static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
     return ppiv;
 }
 
-static void quick_sort_rec(int a[], unsigned int izq, unsigned int der)
-{
-    if (izq < der)
-    {
+static void quick_sort_rec(fixstring a[], unsigned int izq, unsigned int der) {
+    if (izq < der) {
         unsigned int pivot = partition(a, izq, der);
-
-        if (pivot > izq)
-        {
+        if (pivot > izq) {
             quick_sort_rec(a, izq, pivot - 1);
         }
-
-        if (pivot < der)
-        {
+        if (pivot < der) {
             quick_sort_rec(a, pivot + 1, der);
         }
     }
@@ -49,5 +46,3 @@ static void quick_sort_rec(int a[], unsigned int izq, unsigned int der)
 void quick_sort(fixstring a[], unsigned int length) {
     quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
-
-

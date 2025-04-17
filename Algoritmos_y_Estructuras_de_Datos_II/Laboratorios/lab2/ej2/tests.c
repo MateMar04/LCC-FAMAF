4c3fbe32daf86309be85971a93ef8bb2211205ea
Mateo Marchisone <mateo.marchisone@gmail.com>
2025-04-17 13:38:03 -0300

    testing

--- Algoritmos_y_Estructuras_de_Datos_II/Laboratorios/lab2/ej2/tests.c
+++ Algoritmos_y_Estructuras_de_Datos_II/Laboratorios/lab2/ej2/tests.c
@@ -3,74 +3,103 @@
 #include "cima.h"
 
 #define MAX_LENGTH 10
-#define N_TESTCASES_TIENE_CIMA 1
-#define N_TESTCASES_CIMA 1
+#define N_TESTCASES_TIENE_CIMA 10
+#define N_TESTCASES_CIMA 10
 
 void test_tiene_cima(void);
 void test_cima(void);
 
-int main() {
+int main()
+{
     test_tiene_cima();
     test_cima();
 
     return 0;
 }
 
-void test_tiene_cima(void) {
-    struct testcase {
+void test_tiene_cima(void)
+{
+    struct testcase
+    {
         int a[MAX_LENGTH];
         int length;
         bool result;
     };
 
     struct testcase tests[N_TESTCASES_TIENE_CIMA] = {
-      { {8}, 1, true },
-
-        // COMPLETAR!!
+        {{8}, 1, true},
+        {{1, 2, 4, 4, 5}, 5, false},
+        {{5, 4, 3, 2, 1}, 5, true},
+        {{1, 2, 3, 4, 5, 4}, 6, true},
+        {{1, 2, 3, 4, 5, 6}, 6, true},
+        {{6, 5, 4, 3, 2}, 5, true},
+        {{1, 2, 3, 4, 5, 4}, 6, true},
+        {{1}, 1, true},
+        {{-1}, 1, true},
+        {{-1, -2, -3, -4, -5}, 5, true}
 
     };
     bool result;
 
     printf("TESTING tiene_cima\n");
 
-    for (int i=0; i < N_TESTCASES_TIENE_CIMA; i++) {
-        printf("Test case %i: ", i+1);
+    for (int i = 0; i < N_TESTCASES_TIENE_CIMA; i++)
+    {
+        printf("Test case %i: ", i + 1);
 
         result = tiene_cima(tests[i].a, tests[i].length);
 
-        if (result != tests[i].result) {
+        if (result != tests[i].result)
+        {
             printf("FAILED\n");
-        } else {
+        }
+        else
+        {
             printf("OK\n");
         }
     }
 }
 
-void test_cima(void) {
-    struct testcase {
+void test_cima(void)
+{
+    struct testcase
+    {
         int a[MAX_LENGTH];
         int length;
         int result;
     };
 
     struct testcase tests[N_TESTCASES_CIMA] = {
-        { {8}, 1, 0 },
+        {{8}, 1, 8},
+        {{1, 2, 4, 4, 5}, 5, -1},
+        {{5, 4, 3, 2, 1}, 5, 5},
+        {{1, 2, 3, 4, 5, 4}, 6, 5},
+        {{1, 2, 3, 4, 4, 6}, 6, -1},
+        {{6, 5, 4, 3, 2}, 5, 6},
+        {{1, 2, 3, 4, 5}, 5, 5},
+        {{1}, 1, 1},
+        {{-1}, 1, -1},
+        {{-1, -2}, 2, -1}
 
-        // COMPLETAR!!
+        
 
     };
     int result;
 
     printf("TESTING cima\n");
 
-    for (int i=0; i < N_TESTCASES_CIMA; i++) {
-        printf("Test case %i: ", i+1);
+    for (int i = 0; i < N_TESTCASES_CIMA; i++)
+    {
+        printf("Test case %i: ", i + 1);
 
         result = cima(tests[i].a, tests[i].length);
 
-        if (result == tests[i].result) {
+        if (result == tests[i].result)
+        {
             printf("OK\n");
-        } else {
+        }
+        else
+        {
             printf("FAILED. got %d expected %d\n", result, tests[i].result);
         }
     }

842999ba9f4355c2041372e4407cc60fb75cd9a5
Mateo Marchisone <mateo.marchisone@gmail.com>
2025-04-21 11:29:29 -0300

    lab3 ej1

--- Algoritmos_y_Estructuras_de_Datos_II/Laboratorios/lab3/ej1/weather.c
+++ Algoritmos_y_Estructuras_de_Datos_II/Laboratorios/lab3/ej1/weather.c
@@ -2,39 +2,41 @@
   @file weather.c
   @brief Implements weather mesuarement structure and methods
 */
+#include <stdio.h>
 #include <stdlib.h>
 #include "weather.h"
 
 Weather weather_from_file(FILE *file)
 {
-  Weather w;
-  unsigned int year, month, day;
-  int avg, max, min;
-  unsigned int press, moist, rain;
+    Weather w;
+    unsigned int year, month, day;
+    int avg, max, min;
+    unsigned int press, moist, rain;
 
-  // Leemos las 9 columnas: año, mes, día, temp_prom, temp_max, temp_min, presión, humedad, precipitaciones
-  int scanned = fscanf(file,
-                       " %u %u %u  " // año mes día
-                       "%d %d %d  "  // temp_media temp_max temp_min
-                       "%u %u %u",   // presión humedad precipitaciones
-                       &year, &month, &day,
-                       &avg, &max, &min,
-                       &press, &moist, &rain);
-  if (scanned != 9)
-  {
-    fprintf(stderr, "Error: formato de línea meteorológica inválido (se esperaban 9 campos, leídos %d)\n",
+    
+    int scanned = fscanf(file,
+        " %u %u %u "     
+        "%d %d %d "      
+        "%u %u %u",      
+        &year, &month, &day,
+        &avg, &max, &min,
+        &press, &moist, &rain
+    );
+    if (scanned != 9) {
+        fprintf(stderr,
+            "Error: formato de línea meteorológica inválido (se esperaban 9 campos, leídos %d)\n",
             scanned);
-    exit(EXIT_FAILURE);
-  }
+        exit(EXIT_FAILURE);
+    }
 
-  w._average_temp = avg;
-  w._max_temp = max;
-  w._min_temp = min;
-  w._pressure = press;
-  w._moisture = moist;
-  w._rainfall = rain;
+    w._average_temp = avg;
+    w._max_temp     = max;
+    w._min_temp     = min;
+    w._pressure     = press;
+    w._moisture     = moist;
+    w._rainfall     = rain;
 
-  return w;
+    return w;
 }
 
 void weather_to_file(FILE *file, Weather weather)

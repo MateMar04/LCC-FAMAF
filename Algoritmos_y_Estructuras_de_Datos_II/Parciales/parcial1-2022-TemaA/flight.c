e29c2d2d42ed267cc8147dd5c2627cc00e7d65aa
Mateo Marchisone <mateo.marchisone@gmail.com>
2025-04-23 16:46:41 -0300

    Parcial1-2022-TemaA

---
+++ Algoritmos_y_Estructuras_de_Datos_II/Parciales/parcial1-2022-TemaA/flight.c
@@ -0,0 +1,23 @@
+/*
+  @file layover.c
+  @brief Implements flight structure and methods
+*/
+#include <stdlib.h>
+#include "flight.h"
+
+static const int AMOUNT_OF_FLIGHT_VARS = 3 ;
+
+Flight flight_from_file(FILE* file, char code)
+{
+    Flight flight;
+    flight.code = code;
+
+    int data = fscanf(file, "%u %u %u", &flight.type, &flight.hour, &flight.passengers_amount);
+    if (data != AMOUNT_OF_FLIGHT_VARS)
+    {
+        fprintf(stderr, "Error: invalid flight data format\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return flight;
+}

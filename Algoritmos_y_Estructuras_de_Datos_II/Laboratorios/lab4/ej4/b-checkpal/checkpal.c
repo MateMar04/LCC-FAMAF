f69b0e9625537f143d1017e623440954ae8c5933
Mateo Marchisone <mateo.marchisone@gmail.com>
2025-05-12 18:09:33 +0000

    Ej4 A and B

--- Algoritmos_y_Estructuras_de_Datos_II/Laboratorios/lab4/ej4/b-checkpal/checkpal.c
+++ Algoritmos_y_Estructuras_de_Datos_II/Laboratorios/lab4/ej4/b-checkpal/checkpal.c
@@ -5,17 +5,22 @@
 
 #include "strfuncs.h"
 
-#define MAX_LENGTH 20
+#define MAX_LENGTH 100
 
 #define SIZEOF_ARRAY(s) (sizeof(s) / sizeof(*s))
 
 int main(void) {
     char user_input[MAX_LENGTH];
-    char ignore_chars[] = {' ', '?', '!', ',', '-', '.'};
+    char ignore_chars[] = {' ', '?', '!', ',', '-', '.', '\n'};
     char *filtered=NULL;
 
     printf("Ingrese un texto (no más de %d símbolos) para verificar palíndromo: ", MAX_LENGTH);
-    scanf("%s", user_input);
+    fgets(user_input, MAX_LENGTH, stdin);
+
+    //sacnf corta la cadena en el primer espacio.
+    //sacnf("%s" user_input)
+    //printf("%s", user_input); //El problema es que 
+    
     filtered = string_filter(user_input, ignore_chars[0]);
     for (unsigned int i=0; i < SIZEOF_ARRAY(ignore_chars); i++) {
         filtered = string_filter(filtered, ignore_chars[i]);
---
+++ Algoritmos_y_Estructuras_de_Datos_II/Laboratorios/lab4/ej4/b-checkpal/strfuncs.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "strfuncs.h"
+
+size_t string_length(const char *str) {
+    size_t len = 0u;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+char *string_filter(const char *str, char c) {
+    size_t len = string_length(str);
+
+    size_t count = 0u;
+    for (size_t i = 0; i < len; i++) {
+        if (str[i] != c) {
+            count++;
+        }
+    }
+
+    char *result = malloc((count + 1) * sizeof(char));
+
+    size_t j = 0u;
+    for (size_t i = 0; i < len; i++) {
+        if (str[i] != c) {
+            result[j++] = str[i];
+        }
+    }
+
+    result[j] = '\0';
+    return result;
+}
+
+bool string_is_symmetric(const char *str) {
+    size_t len = string_length(str);
+    bool result = true;
+    for (size_t i = 0u; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            result = false;
+        }
+    }
+    return result;
+}

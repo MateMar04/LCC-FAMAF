ae5e35bed84c6a752bc11af9a74afb4ce6f49f8c
Mateo Marchisone <mateo.marchisone@gmail.com>
2025-05-15 19:05:14 +0000

    Ej2

--- Algoritmos_y_Estructuras_de_Datos_II/Laboratorios/lab4-2/ej2/main.c
+++ Algoritmos_y_Estructuras_de_Datos_II/Laboratorios/lab4-2/ej2/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define LINKED_LIST_SIZE 3
 
 typedef struct Node {
     int data;
@@ -12,9 +13,29 @@ typedef Node *List;
  * @brief Construye y devuelve una lista de ejemplo de 3 elementos
  */
 List setup_example() {
-    //
-    // COMPLETAR ACÁ!
-    //
+    int i = LINKED_LIST_SIZE;
+
+    // Crear primer nodo
+    List my_list = malloc(sizeof(Node));
+
+    Node *a_node = my_list;
+    i--;
+
+    while (i > 0) {
+        a_node->data = i * 10;
+
+        // Reservar memoria para el siguiente nodo
+        a_node->next = malloc(sizeof(Node));
+
+        a_node = a_node->next;
+        i--;
+    }
+
+    // Último nodo
+    a_node->data = 0;
+    a_node->next = NULL;
+
+    return my_list;
 }
 
 void show_list(List xs) {
